24484169a0bf9ae3e23d68256f427ee75064abd8
Hsiao Feng <vectorst@foxmail.com>
2021-07-25 23:27:07 +0800

    解决一些问题。

--- jump.js
+++ jump.js
@@ -1,5 +1,7 @@
-if(!(new RegExp("^(http://)|(https://)|(ftp://).*").test(url))){
-  window.location.assign("http://"+url);
-}else{
-  window.location.assign(url);
+let urlArr = new RegExp("(^|&)pfurl=([^&]*)(&|$)","i");
+let result = window.location.search.substr(1).match(urlArr);
+let TargetUrl = decodeURIComponent(result[2]);
+if(!(new RegExp("^(http://)|(https://)|(ftp://).*").test(TargetUrl))){
+    TargetUrl = "http://"+decodeURIComponent(result[2]);
 }
+window.location.assign(TargetUrl);
