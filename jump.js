075c319b8dcac42b095cda381b23e757c7e4650e
Hsiao Feng <vectorst@foxmail.com>
2021-07-25 22:01:51 +0800

    优化代码逻辑
    Thanks @chenjunyu19

--- jump.js
+++ jump.js
@@ -1,7 +1,5 @@
-var urlArr = new RegExp("(^|&)pfurl=([^&]*)(&|$)","i");
-var result = window.location.search.substr(1).match(urlArr);
-var url = decodeURIComponent(result[2]);
 if(!(new RegExp("^(http://)|(https://)|(ftp://).*").test(url))){
-    url = "http://"+decodeURIComponent(result[2]);
+  window.location.assign("http://"+url);
+}else{
+  window.location.assign(url);
 }
-window.location.assign(url);
